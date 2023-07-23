3c12bcfd0b61342922ed94301e31299217a279e1
psamonwong <phrutsamon.wongnak@gmail.com>
2023-07-23 21:20:47 +0700

    Swabber variations

--- Rcodes models/mod3_swabber.R
+++ Rcodes models/mod3_swabber.R
@@ -5,6 +5,10 @@ getwd()
 mainDir <- "D:/Determinants-viral-clearance"
 setwd(mainDir)
 data <- read.csv("Analysis_Data/swabber_analysis.csv")
+data <- data[!data$Swabber == "-",]
+data$Swabber_key <- as.factor(data$Swabber)
+data$Swabber <- as.numeric(data$Swabber_key)
+
 data$ID_code <- data$ID
 data$ID <- as.numeric(as.factor(data$ID))
 data <- data[order(c(data$censor), decreasing = T),]
@@ -20,10 +24,13 @@ Time_max <- max(data$Timepoint_ID)
 obs_day <- data$Timepoint_ID
 RNaseP <- as.vector(scale(data$CT_RNaseP))
 
-#alpha_0_prior_mean <- 5
-#alpha_0_prior_sd <- 2
-#beta_0_prior_mean <- -0.5
-#beta_0_prior_sd <- 1
+n_swabber <- length(unique(data$Swabber))
+swabber_id <- data$Swabber
+
+alpha_0_prior_mean <- 5
+alpha_0_prior_sd <- 2
+beta_0_prior_mean <- -0.5
+beta_0_prior_sd <- 1
 sigma_logvl_mean <- 1
 sigma_logvl_sd <- 1
 
@@ -39,19 +46,21 @@ data_for_stan <- list(
   Time_max = Time_max,
   obs_day = obs_day,
   RNaseP = RNaseP,
+  n_swabber = n_swabber,
+  swabber_id = swabber_id,
   
   #priors
   #alpha_0_prior_mean = alpha_0_prior_mean,
   #alpha_0_prior_sd = alpha_0_prior_sd,
   #beta_0_prior_mean = beta_0_prior_mean,
-  #beta_0_prior_sd = beta_0_prior_sd,
+  #beta_0_prior_sd = beta_0_prior_sd#,
   sigma_logvl_mean = sigma_logvl_mean,
   sigma_logvl_sd = sigma_logvl_sd
   
 )
 #############################################################################
 # Running stan code
-model <- stan_model("Stan_models/TwoCmpt_for_ineffective_arm.stan",verbose = T)
+model <- stan_model("Stan_models/TwoCmpt_for_ineffective_arm_swabber.stan",verbose = T)
 
 fit <- sampling(
   object = model,         # Stan model
@@ -61,10 +70,14 @@ fit <- sampling(
   iter = 4000,            # total number of iterations per chain
   thin = 2,
   cores =4,              # number of cores (could use one per chain)
-  refresh = 500
+  refresh = 200
 )
 
-fit
-save(fit, file = "Swab variations/Fit/TwoCmpt_nRNaseP.RData")
+summary(fit, par = "sigma_logvl", prob = c(0.025, 0.5, 0.975))
 
-traceplot(fit, pars = c("loglambda1_0", "loglambda2_0", "logB0_0", "logA0_0"))
+
+save(fit, file = "Fit/TwoCmpt_nRNaseP_swabber.RData")
+
+traceplot(fit, pars = c("sigma_logvl"))
+
+
