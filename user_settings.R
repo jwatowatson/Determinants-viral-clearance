cf0a691e987e9cf4fa76a6c14534da15d105ad8b
psamonwong <137978901+psamonwong@users.noreply.github.com>
2024-04-10 11:13:49 +0700

    Delete user_settings.R

--- user_settings.R
+++
@@ -1,7 +0,0 @@
-# gitignore this file
-
-prefix_analysis_data <- ".."
-prefix_dropbox <- "~/Dropbox/MORU/Adaptive Trials/PLATCOV_Analysis"
-prefix_downloads <- "~"
-prefix_drop_rand <- "~/Dropbox/PLATCOV"
-prefix_dat_cur <- ""
